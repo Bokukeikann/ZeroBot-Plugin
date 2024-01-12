a8458abf34c0b973775901433c76e779c710a63a
Bokukeikann <156408027+Bokukeikann@users.noreply.github.com>
2024-01-12 19:04:44 +0800

    Update main.go

--- main.go
+++ main.go
@@ -258,8 +258,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	sus = append(sus, 1259103229)
-	// sus = append(sus, 87654321)
+	 sus = append(sus, 1259103229)
+	 sus = append(sus, 3489767258)
 
 	// 启用 webui
 	// go webctrl.RunGui(*g)
